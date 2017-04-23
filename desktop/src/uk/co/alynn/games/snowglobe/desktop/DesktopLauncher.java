c1f7ec6c847e7df09378990afa68b3f1ec909104
Alistair Lynn <arplynn@gmail.com>
2017-04-23 11:53:13 +0100

    Initial commit

---
+++ desktop/src/uk/co/alynn/games/snowglobe/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package uk.co.alynn.games.snowglobe.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import uk.co.alynn.games.snowglobe.Snowglobe;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Snowglobe(), config);
+	}
+}
