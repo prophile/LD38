b19535082746de2b1f7313b8a09cabef4cde71d5
Alistair Lynn <arplynn@gmail.com>
2017-04-23 12:52:02 +0100

    Abstract base game mode

---
+++ core/src/uk/co/alynn/games/snowglobe/AbstractGameMode.java
@@ -0,0 +1,23 @@
+package uk.co.alynn.games.snowglobe;
+
+public class AbstractGameMode implements GameMode {
+    @Override
+    public void tick(double dt) {
+    }
+
+    @Override
+    public void preActivate() {
+    }
+
+    @Override
+    public void postActive() {
+    }
+
+    @Override
+    public void preDeactivate() {
+    }
+
+    @Override
+    public void postDeactivate() {
+    }
+}
