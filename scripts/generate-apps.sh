eeb027403f35336c65681b34a0f84d753744da91
Alistair Lynn <arplynn@gmail.com>
2017-04-23 12:06:57 +0100

    Make this actually work

--- scripts/generate-apps.sh
+++ scripts/generate-apps.sh
@@ -4,21 +4,21 @@ mkdir -p distributions/final
 mkdir -p distributions/windows
 mkdir -p distributions/mac
 java -jar jres/packr.jar \
-     -platform windows \
-     -jdk 'jres/win32.zip' \
-     -executable snowglobe \
-     -appjar desktop/build/libs/desktop-1.0.jar \
-     -mainclass uk/co/alynn/games/snowglobe/desktop/DesktopLauncher \
-     -minimizejre hard \
-     -outdir distributions/windows/snowglobe
+     --platform windows32 \
+     --jdk 'jres/win32.zip' \
+     --executable snowglobe \
+     --classpath desktop/build/libs/desktop-1.0.jar \
+     --mainclass uk.co.alynn.games.snowglobe.desktop.DesktopLauncher \
+     --minimizejre hard \
+     --output distributions/windows/snowglobe
 java -jar jres/packr.jar \
-     -platform mac \
-     -jdk 'jres/mac64.zip' \
-     -executable Snowglobe \
-     -appjar desktop/build/libs/desktop-1.0.jar \
-     -mainclass uk/co/alynn/games/snowglobe/desktop/DesktopLauncher \
-     -minimizejre hard \
-     -outdir distributions/mac/Snowglobe.app
+     --platform mac \
+     --jdk 'jres/mac64.zip' \
+     --executable Snowglobe \
+     --classpath desktop/build/libs/desktop-1.0.jar \
+     --mainclass uk.co.alynn.games.snowglobe.desktop.DesktopLauncher \
+     --minimizejre hard \
+     --output distributions/mac/Snowglobe.app
 pushd distributions/windows
 zip -r ../final/snowglobe-win32.zip snowglobe
 popd
